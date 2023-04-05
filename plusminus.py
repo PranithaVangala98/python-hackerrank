a9dcb880d580e3faa0f734bf8940bb0b6e9fa786
Pranitha Vangala <pranithavangala1998@gmail.com>
2023-04-05 13:18:49 +0530

    plusminus file

---
+++ plusminus.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+def plusMinus(arr):
+    # Write your code here
+       # Write your code here
+    count_positive = 0
+    count_negative = 0
+    count_zero = 0
+    deci_negative = 0
+    deci_positive = 0
+    deci_zero = 0
+    for i in arr:
+      if i == 0 :
+        count_zero+=1
+      elif i > 0:
+        count_positive +=1
+      elif i < 0:
+        count_negative +=1
+    n = count_positive+count_negative+count_zero
+    deci_positive = count_positive/n
+    deci_negative = count_negative/n
+    deci_zero = count_zero/n
+    print("{:.6f}".format(deci_positive))
+    print("{:.6f}".format(deci_negative))
+    print("{:.6f}".format(deci_zero))
+    
+
+arr = [1,2,0,0,-6,-6]
+plusMinus(arr)
