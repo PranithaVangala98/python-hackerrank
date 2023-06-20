a9183d7d3543f8e42b8b966d0817d9701b0fcce2
Pranitha Vangala <pranithavangala1998@gmail.com>
2023-06-20 11:42:27 +0530

    matchingStrings

---
+++ matchingStrings.py
@@ -0,0 +1,14 @@
+def matchingStrings(str,queries):
+    ar =[]
+    for i in range(len(queries)):
+        ar.append(0) 
+    for i in range(0,len(queries)):
+        count = 0
+        for j in range(0,len(str)):
+            if queries[i] == str[j]:
+                count+=1
+        ar[i] += count
+    return ar
+str = ['ab','ab','abc']
+queries = ['ab','abc','bc']
+matchingStrings(str,queries) 
