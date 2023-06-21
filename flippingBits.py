1b13b1ca4c0218e280b2292215b7ffb774d2d68a
Pranitha Vangala <pranithavangala1998@gmail.com>
2023-06-21 12:11:08 +0530

    modified

--- flippingBits.py
+++ flippingBits.py
@@ -1,26 +1,32 @@
+
 def flippingBits(n):
-    p = []
-    integer =[]
+    def binay_to_integer(b):
+        sum = 0
+        r = b[::-1]
+        for i in range(len(b)):
+            sum+=(2**i)*int(r[i])
+        return sum
+
     b = 0
-    final_number = 0
     b = format(n,"032b")
-    for i in range(32):
-        p.append(0)
-        integer.append(0)
-    for i in range(32):
-        integer[i] += int(b[i]) 
-    for i in range(32):
-        if integer[i] == 1:
-            p[i]+= 0
+    print(type(b),b)
+    flip_str = ''
+    for i in b:
+        if int(i) == 0:
+            flip_str+='1'
         else:
-            p[i]+=1
-    for i in p:
-        final_number = final_number*2+int(i)
-    return final_number
-       
-n = 2147483647
+            flip_str+='0'
+    print('flipstr',flip_str)
+    
+    return binay_to_integer(flip_str)
+      
+n = 9
 print(flippingBits(n))
 
+ # for i in p:
+    #     final_number = final_number*2+int(i)
+    # return final_number
+       
 
 
      
