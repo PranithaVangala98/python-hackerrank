dc1abae0bf9e55554ef2f6d0c453ae1715b3fc61
Pranitha Vangala <pranithavangala1998@gmail.com>
2023-06-27 14:48:43 +0530

    subarray

---
+++ subArrays.py
@@ -0,0 +1,11 @@
+def birthdays(s,d,m):
+    count = 0
+    for i in range(len(s) - m + 1):
+        if sum(s[i:i+m]) == d:
+            count += 1
+    return count
+    
+s =[2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1]
+d = 18
+m = 7
+print(birthdays(s,d,m))
